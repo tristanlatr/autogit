b4683756b824117f383293fe96470a62c2cd96fe
tristanlatr <tris.la.tr@gmail.com>
2020-03-02 10:42:38 -0500

    [git-admin] Changes on Tristans-MBP Mon Mar  2 10:42:38 EST 2020
    git-admin v1

--- git-admin.sh
+++ git-admin.sh
@@ -4,8 +4,6 @@
 symbol="*"
 paddingSymbol=" "
 lineLength=70
-charsToOption=1
-charsToName=3
 function generatePadding() {
     string="";
     for (( i=0; i < $2; i++ )); do
@@ -15,7 +13,6 @@ function generatePadding() {
 }
 remainingLength=$(( $lineLength - 2 ));
 line=$(generatePadding "${symbol}" "${lineLength}");
-# generateText (text)
 function generateTitle() {
     totalCharsToPad=$((remainingLength - ${#1}));
     charsToPadEachSide=$((totalCharsToPad / 2));
@@ -38,8 +35,7 @@ usage(){
     generateTitle "Usage"
     echo "Usage: $0 [-h] [-k <Key auth for git repo>] [-c <git remote URL>] (-r <Repositorie(s) path(s)>) [-b <Branch>] [-u <Strategy>] [-t <Commit hash>] [-i <Number of commits to show>]" | fold -s
     echo
-    echo "This script is designed to manage versionning git repository." | fold -s
-    echo "VERY IMPORTANT. Arguments should be passed to the script in the same order they are listed in this message to avoid unexpected behaviours." | fold -s
+    echo "This script is designed to programatically manage merge, pull and push feature on git repository." | fold -s
     echo
     echo -e "\t-h\t\tPrint this help message." | fold -s
     echo -e "\t-k <Key>\tPath to a trusted ssh key to authenticate against the git server (push). Required if git authentication is not already working with default key." | fold -s
@@ -47,7 +43,7 @@ usage(){
     echo -e "\t-r <Paths>\tPath to managed repository, can be multiple comma separated. Warning make sure all repositories exists., multiple repo values are not supported by the git clone feature '-c'. Repository path(s) should end with the default git repo folder name after git clone Required." | fold -s
     echo -e "\t-b <Branch>\tSwitch to the specified branch or tag." | fold -s
     echo -e "\t\t\tBranch must already exist in the local repository copy (run git checkout origin/branch from the host before)." | fold -s
-    echo -e "\t-u <Strategy>\t\tUpdate the current branch from and to upstream, can adopt 3 strategies. This feature supports multiple repo values !" | fold -s
+    echo -e "\t-u <Strategy>\tUpdate the current branch from and to upstream, can adopt 3 strategies. This feature supports multiple repo values !" | fold -s
     echo -e "\t\t'merge' -> commit, pull and push. Fail if merge fail. Require valid git server authentication." | fold -s
     echo -e "\t\t'stash' -> stash the changes and pull." | fold -s
     echo -e "\t\t'merge-or-stash' -> stash, commit, pull and push, if pull fails revert commit and pull. Require valid git server authentication." | fold -s
@@ -58,11 +54,11 @@ usage(){
     echo -e "\t-i <Number of commits to show>\tShows informations." | fold -s
     echo
     echo -e "\tExamples : " | fold -s
-    echo -e "\t\t$0 -r ~/isrm-portal-conf/ -b stable -u -i 5" | fold -s
+    echo -e "\t\t$0 -r ~/isrm-portal-conf/ -b stable -u merge -i 5" | fold -s
     echo -e "\t\tCheckout the stable branch, pull changes and show infos of the repository (last 5 commits)." | fold -s
     echo -e "\t\t$0 -r ~/isrm-portal-conf/ -b stable -t 00a3a3f" | fold -s
     echo -e "\t\tCheckout the stable branch and hard reset the repository to the specified commit." | fold -s
-    echo -e "\t\t$0 -k ~/.ssh/id_rsa2 -c git@github.com:mfesiem/msiempy.git -r ./test/msiempy/ -u " | fold -s
+    echo -e "\t\t$0 -k ~/.ssh/id_rsa2 -c git@github.com:mfesiem/msiempy.git -r ./test/msiempy/ -u merge-or-stash " | fold -s
     echo -e "\t\tInit a repo and pull master by default. Use the specified SSH to authenticate." | fold -s
     echo
     echo -e "\tReturn codes : "
@@ -71,6 +67,7 @@ usage(){
     echo -e "\t\t3 Syntax mistake"
     echo -e "\t\t4 Git reposirtory does't exist and -c URL is not set"
     echo -e "\t\t5 Repository not set"
+    echo -e "\t\t6 Can't checkout with unstaged files in working tree"
 }
 
 git_ssh(){
@@ -139,7 +136,7 @@ while getopts "${optstring}" arg; do
 
         f)
             commit_msg_file=${OPTARG}
-            generateTitle "Commit message file set : ${commit_msg_file}"
+            generateTitle "Commit message set ${commit_msg_file}"
             ;;
     esac
 done
@@ -158,18 +155,15 @@ while getopts "${optstring}" arg; do
                     git_ssh "git branch -a -vv" "${ssh_key}"
                 else
                     if [[ ! -z "${git_clone_url}" ]]; then
-                        
-                        cd `dirname ${folder}`
-                        mkdir `basename ${folder}`
-                        cd `basename ${folder}`
-
+                        echo "[INFO] Repository do no exist, initating it."
+                        mkdir -p ${folder}
+                        cd ${folder}
                         git init
                         git remote add -t master origin ${git_clone_url} 
-
                         git_ssh "git remote update" "${ssh_key}"
                         git_ssh "git branch -a -vv" "${ssh_key}"
                     else
-                        echo "[ERROR] Git reposirtory does't exist and -c URL is not set. Please make sure arguments are in the correct order."
+                        echo "[ERROR] Git reposirtory do not exist and -c URL is not set. Please set git URL to be able to initiate the repo" |  fold -s
                         exit 4
                     fi
                 fi
@@ -180,23 +174,22 @@ while getopts "${optstring}" arg; do
     esac
 done
 OPTIND=1
+if [[ "$repositoryIsSet" = false ]]; then
+    echo "[ERROR] You need to set the repository -r <Path> to continue."
+    exit 5
+fi 
 while getopts "${optstring}" arg; do
     case "${arg}" in
         t) #Reseting to previous commit
-            if [ "$repositoryIsSet" = true ]; then
-                for folder in ${repositories}; do
-                    generateTitle "[INFO] Reseting ${folder} to ${OPTARG} commit"
-                    cd $folder
-                    git reset --hard ${OPTARG}
-                    cd "${init_folder}"
-                    break
-                done
-                generateTitle "End (reset)"
-                exit
-            else
-                echo "[ERROR] You need to set the repository to reset branch"
-                exit 5
-            fi
+            for folder in ${repositories}; do
+                generateTitle "[INFO] Reseting ${folder} to ${OPTARG} commit"
+                cd $folder
+                git reset --hard ${OPTARG}
+                cd "${init_folder}"
+                break
+            done
+            generateTitle "End (reset)"
+            exit
             ;;
     esac
 done
@@ -204,17 +197,18 @@ OPTIND=1
 while getopts "${optstring}" arg; do
     case "${arg}" in
         b) #Checkout
-            if [ "$repositoryIsSet" = true ]; then
-                for folder in ${repositories}; do
-                    generateTitle "Checkout ${folder} on branch ${OPTARG}"
-                    cd $folder
+            for folder in ${repositories}; do
+                generateTitle "Checkout ${folder} on branch ${OPTARG}"
+                cd $folder
+                if git diff-files --quiet --
+                then
                     git checkout ${OPTARG}
-                    cd "${init_folder}"
-                done
-            else
-                echo "[ERROR] You need to set the repository to checkout a branch"
-                exit 5
-            fi
+                else
+                    echo "[ERROR] Can't checkout with unstaged files in working tree, please merge changes first." | fold -s
+                    exit 6
+                fi
+                cd "${init_folder}"
+            done
             ;;
     esac
 done
@@ -222,84 +216,81 @@ OPTIND=1
 while getopts "${optstring}" arg; do
     case "${arg}" in
         u) #Update
-            if [ "$repositoryIsSet" = true ]; then
-                for folder in ${repositories}; do
-                    
-                    cd $folder
-                    generateTitle "Updating ${folder}"
-                    local_changes=0
-                    strategy=${OPTARG}
-                    git_status=`git status -s`
-                    commit_and_stash_name="git-admin changes on ${host} $(date)"
+            for folder in ${repositories}; do
+                
+                cd $folder
+                generateTitle "Updating ${folder}"
+                local_changes=0
+                strategy=${OPTARG}
 
-                    if [[ -n ${git_status} ]]; then
+                if [[ ! "${strategy}" =~ "merge" ]] && [[ ! "${strategy}" =~ "stash" ]]; then
+                    echo "[ERROR] Unkwown strategy ${strategy} '-u <Strategy>' option argument. Please see $0 '-h' for more infos." | fold -s
+                    exit 3
+                fi
 
-                        if [[ "${strategy}" =~ "add-untracked" ]]; then
-                            echo "[INFO] Adding untracked files"
-                            git add .
-                        fi
-                        echo "[INFO] Locally changed files:"
-                        git status -s
+                if [[ -n `git status -s` ]]; then
 
-                        if [[ "${strategy}" =~ "stash" ]];then
-                            # If unstaged changes in the working tree
-                            if ! git diff-files --quiet --ignore-submodules --
-                            then
-                                echo "[INFO] Saving changes as a git stash, please apply stash manually from ${host} with 'git stash pop' if you need."
-                                
-                                git stash save "${commit_and_stash_name}"
-                                if [[ "${strategy}" =~ "or-stash" ]]; then
-                                    echo "[INFO] Applying stash in order to merge"
-                                    git stash apply --quiet stash@{0}
-                                fi
+                    commit_and_stash_name="[git-admin] Changes on ${host} $(date)"
+
+                    if [[ "${strategy}" =~ "add-untracked" ]]; then
+                        echo "[INFO] Adding untracked files"
+                        git add .
+                    fi
+                    echo "[INFO] Locally changed files:"
+                    git status -s
+
+                    if [[ "${strategy}" =~ "stash" ]];then
+                        # If unstaged changes in the working tree
+                        if ! git diff-files --quiet --
+                        then
+                            echo "[INFO] Saving changes as a git stash, please apply stash manually from ${host} with 'git stash pop' if you need." | fold -s
+                            git stash save "${commit_and_stash_name}"
+                            if [[ "${strategy}" =~ "merge-or-stash" ]]; then
+                                echo "[INFO] Applying stash in order to merge"
+                                git stash apply --quiet stash@{0}
                             fi
                         fi
+                    fi
 
-                        if [[ "${strategy}" =~ "merge" ]]; then
-                            # If unstaged changes in the working tree
-                            if ! git diff-files --quiet --ignore-submodules --
-                            then
-                                echo "[INFO] Merging changes"
-                                if [[ -n "${commit_msg_file}" ]]; then
-                                    commit_msg_file_text=`cat "${commit_msg_file}"`
-                                    git commit -a -m "${commit_and_stash_name}" -m "${commit_msg_file_text}"
-                                else
-                                    git commit -a -m "${commit_and_stash_name}"
-                                fi
-                                local_changes=1
+                    if [[ "${strategy}" =~ "merge" ]]; then
+                        # If unstaged changes in the working tree
+                        if ! git diff-files --quiet --
+                        then
+                            echo "[INFO] Merging changes"
+                            if [[ -n "${commit_msg_file}" ]]; then
+                                commit_msg_file_text=`cat "${commit_msg_file}"`
+                                git commit -a -m "${commit_and_stash_name}" -m "${commit_msg_file_text}"
+                            else
+                                git commit -a -m "${commit_and_stash_name}"
                             fi
+                            local_changes=1
                         fi
+                    fi
+                else
+                    echo "[INFO] No local changes"
+                fi
 
-                        if [[ ! "${strategy}" =~ "merge" ]] && [[ ! "${strategy}" =~ "stash" ]]; then
-                            echo "[ERROR] Unkwown strategy ${strategy} '-u <Strategy>' option argument. Please see $0 '-h' for more infos."
-                            exit 3
-                        fi
+                echo "[INFO] Pulling changes"
+                if ! git_ssh "git pull" "${ssh_key}"
+                then
+                    if [[ "${strategy}" =~ "merge-or-stash" ]]; then
+                        echo "[WARNING] Git pull failed. Reseting to last commit."
+                        echo "[INFO] Your changes are saved as git stash \"${commit_and_stash_name}\"" | fold -s
+                        git reset --hard HEAD~1
+                        echo "[INFO] Pulling changes"
+                        git_ssh "git pull" "${ssh_key}"
+                    else
+                        echo "[ERROR] Git pull failed: please read error output. You can merge manually or use another update stategy. You can also hard reset to previous commit using '-t' option, your local changes will be erased." | fold -s
+                        exit 2
                     fi
-                    echo "[INFO] Pulling changes"
-                    if ! git_ssh "git pull" "${ssh_key}"
-                    then
-                        if [[ "${strategy}" =~ "or-stash" ]]; then
-                            echo "[WARNING] Git pull failed. Reseting to last commit."
-                            echo "[INFO] Your changes are saved as git stash \"${commit_and_stash_name}\""
-                            git reset --hard HEAD~1
-                            echo "[INFO] Pulling changes"
-                            git_ssh "git pull" "${ssh_key}"
-                        else
-                            echo "[ERROR] Git pull failed: please read error output. You can merge manually or use another update stategy. You can also hard reset to previous commit using '-t' option, your local changes will be erased."
-                            exit 2
-                        fi
-                    fi
+                fi
 
-                    if [[ $local_changes -eq 1 ]]; then
-                        echo "[INFO] Pushing changes"
-                        git_ssh "git push" "${ssh_key}"
-                    fi
-                    cd "${init_folder}"
-                done
-            else
-                echo "[ERROR] You need to set the repository to update"
-                exit 5
-            fi
+                if [[ $local_changes -eq 1 ]]; then
+                    echo "[INFO] Pushing changes"
+                    git_ssh "git push" "${ssh_key}"
+                fi
+                cd "${init_folder}"
+            done
             ;;
     esac
 done
@@ -308,22 +299,17 @@ while getopts "${optstring}" arg; do
     case "${arg}" in
         i) #Show git log -> To have the commits sha1
             generateTitle "Informations"
-            if [ "$repositoryIsSet" = true ]; then
-                for folder in ${repositories}; do
-                    cd $folder
-                    generateTitle "Last ${OPTARG} commits activity ${folder}"
-                    git --no-pager log -n ${OPTARG} --graph
-                    #git --no-pager log --graph --all --since "$(date -d "${OPTARG} days ago" "+ %Y-%m-%dT%T")"
-                    generateTitle "Tracked files ${folder}"
-                    git ls-tree --full-tree -r --name-only HEAD
-                    generateTitle "Git status ${folder}"
-                    git status
-                    cd "${init_folder}"
-                done
-            else
-                echo "[ERROR] You need to set the repository to show information"
-                exit 5
-            fi
+            for folder in ${repositories}; do
+                cd $folder
+                generateTitle "Last ${OPTARG} commits activity ${folder}"
+                git --no-pager log -n ${OPTARG} --graph
+                #git --no-pager log --graph --all --since "$(date -d "${OPTARG} days ago" "+ %Y-%m-%dT%T")"
+                generateTitle "Tracked files ${folder}"
+                git ls-tree --full-tree -r --name-only HEAD
+                generateTitle "Git status ${folder}"
+                git status
+                cd "${init_folder}"
+            done
             ;;
     esac
 done
