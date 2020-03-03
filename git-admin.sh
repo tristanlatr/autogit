4c99f7562937317f176680c31c5758362f73cea3
tristanlatr <tris.la.tr@gmail.com>
2020-03-03 11:43:20 -0500

    [git-admin] Changes on Tristans-MBP Tue Mar  3 11:43:20 EST 2020

--- git-admin.sh
+++ git-admin.sh
@@ -351,7 +351,7 @@ while getopts "${optstring}" arg; do
                 else
                     branch=`git rev-parse --abbrev-ref HEAD`
                     echo "[INFO] Clearing stashes of current branch (${branch}), leaving last 5 stashes" | fold -s
-                    for stash in `git stash list | grep "On ${branch}" | awk -F ':' '{print$1}' | tail -n+7`; do
+                    for stash in `git stash list | grep "On ${branch}" | awk -F ':' '{print$1}' | tail -n+7 | tail -r`; do
                         if ! git stash drop --quiet ${stash}
                         then
                             stash_name=`git stash list | grep "On ${branch}" | grep "${stash}"`
